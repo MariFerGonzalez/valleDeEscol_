47416eed82e95e42448631fe84aab574c09cf43a
allenor1090 <99224234+allenor1090@users.noreply.github.com>
2022-04-26 21:21:41 -0500

    cambios
    implementacion de metodos POST,GET,PUT y DELETE para factura, cliente, producto y proveedores.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 const parser= require("body-parser");
 const express = require("express");
 const app= express();
-const port=3000;
-const rutasInicio =require("./routes/iniciar_Sesion");    
+const port=7000;
+//rutas archivos js
+const rutasInicio = require("./routes/iniciar_Sesion");  
+const rutaClientes =require("./routes/cliente");// rutas clientes @Alberto  
+const rutaProducto = require("./routes/producto"); 
+const rutaProvedor=require("./routes/provedores");
+const rutaFactura = require("./routes/facturas");
 const mongoose= require("mongoose");
 require ('dotenv').config();
 
 app.use(parser.urlencoded({extended: false}));
 app.use(parser.json());
-
+//rutas api
 app.use("/api", rutasInicio);
+app.use("/api", rutaClientes);// ruta Api Clientes @Alberto
+app.use("/api",rutaProducto);
+app.use("/api",rutaProvedor);
+app.use("/api", rutaFactura);
 app.use(express.json());
 
 //conexión base de datos
@@ -18,6 +27,7 @@ mongoose
 .then(()=>console.log("conexion exitosa"))
 .catch((error)=>console.log(error));
 //conexion a los puertos
-app.listen(port,()=>{
-  console.log(`Example app listening on port ${port}`)
-});
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  });
+  
